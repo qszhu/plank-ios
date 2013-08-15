bae1c47b091c72c5788a09a121404d95a503b9a3
Qinsi ZHU <qszhu2006@gmail.com>
2013-08-15 09:55:21 +0800

    init commit

---
+++ Plank/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Plank
+//
+//  Created by Qinsi ZHU on 8/15/13.
+//  Copyright (c) 2013 Qinsi ZHU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Plank/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Plank
+//
+//  Created by Qinsi ZHU on 8/15/13.
+//  Copyright (c) 2013 Qinsi ZHU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
